ac84160aed42260f7428d75bea96c35d91159499
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-09-09 19:31:17 +0600

    Second Programm added

---
+++ First-Programm/index.js
@@ -0,0 +1,4 @@
+var userName = "Tonmoy Ahamed";
+var age = 50;
+console.log('Name:' + userName, 'age:' + age);
+// index.ts -> tsc index.ts -> index.js -> node index.js
---
+++ First-Programm/index.ts
@@ -0,0 +1,6 @@
+let userName = "Tonmoy Ahamed"
+let age = 50
+console.log('Name:' + userName, 'age:' + age)
+
+
+// index.ts -> tsc index.ts -> index.js -> node index.js
